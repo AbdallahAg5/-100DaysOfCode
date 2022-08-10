fcdb45bdd965991d8fc3d7cc8d55281f29af0701
AbdallahAg5 <88725504+AbdallahAg5@users.noreply.github.com>
2022-08-10 03:28:12 +0100

    Delete BackgroundColorGenerator directory

--- BackgroundColorGenerator/script.js
+++
@@ -1,9 +0,0 @@
-btn=document.querySelector('.btn')
-
-btn.addEventListener('click',function(){
-    var color1=document.getElementById('firstcolor')
-    var color2=document.getElementById('secondcolor')
-    var result=document.getElementById('result')
-    result.style.backgroundImage=`linear-gradient(45deg ,${color1.value},${color2.value})`
-
-})
